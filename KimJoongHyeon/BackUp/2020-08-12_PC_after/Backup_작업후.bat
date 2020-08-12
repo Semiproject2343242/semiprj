b4953f4e3448c09d8d041fbe94f3b888a6fb1788
JoongHyeon <67622646+JoongHyeon@users.noreply.github.com>
2020-08-12 20:55:09 +0900

    Q/A게시판
    FAQ게시판

---
+++ KimJoongHyeon/BackUp/2020-08-12_PC_after/Backup_작업후.bat
@@ -0,0 +1,9 @@
+rem @echo off
+@echo ========================================
+@echo       Backup
+@echo ========================================
+
+if exist .\exclude.lst set exclude=/exclude:.\exclude.lst
+
+xcopy %exclude% /sedchriy *.*  C:\Users\month\Documents\GitHub\semiprj\KimJoongHyeon\BackUp\%Date%_PC_after
+set exclude=
