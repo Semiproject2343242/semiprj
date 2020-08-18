28f65ca800732ffa4d3396f84ae1d9bf68d8b114
JoongHyeon <67622646+JoongHyeon@users.noreply.github.com>
2020-08-18 21:08:46 +0900

    업데이트중

---
+++ KimJoongHyeon/BackUp/2020-08-18_NetBook_before/Backup.bat
@@ -0,0 +1,10 @@
+rem @echo off
+@echo ========================================
+@echo       Backup
+@echo ========================================
+
+if exist .\exclude.lst set exclude=/exclude:.\exclude.lst
+
+xcopy %exclude% /sedchriy *.* C:\Users\jhkim6\Documents\GitHub\semiprj\KimJoongHyeon\BackUp\%Date%_NetBook
+
+set exclude=
