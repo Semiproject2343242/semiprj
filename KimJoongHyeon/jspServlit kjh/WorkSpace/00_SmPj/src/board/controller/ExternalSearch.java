d060c24c3638cd377ff3c1aadbf48e66f119d581
JoongHyeon <67622646+JoongHyeon@users.noreply.github.com>
2020-09-03 09:02:17 +0900

    통합중

--- KimJoongHyeon/jspServlit kjh/WorkSpace/00_SmPj/src/board/controller/ExternalSearch.java
+++ KimJoongHyeon/jspServlit kjh/WorkSpace/00_SmPj/src/board/controller/ExternalSearch.java
@@ -14,16 +14,16 @@ import board.model.vo.Board;
 import board.model.vo.PageInfo;
 
 /**
- * Servlet implementation class ExternalMainServlet
+ * Servlet implementation class ExternalSearch
  */
-@WebServlet("/main.ea")
-public class ExternalMainServlet extends HttpServlet {
+@WebServlet("/search.ea")
+public class ExternalSearch extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ExternalMainServlet() {
+    public ExternalSearch() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,53 +32,49 @@ public class ExternalMainServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String ea_category = request.getParameter("ea_category");
+		String ea_category = request.getParameter("ea_category");
 		String ck_ea_age = request.getParameter("ck_ea_age");
 		String ck_lc = request.getParameter("ck_lc");
-		int result = 0;
-		int count = Integer.parseInt(request.getParameter("count"));
-		result=result + count;*/
+		System.out.println("카테고리야"+ck_lc);
+		System.out.println("카테고리야"+ea_category);
+		System.out.println("카테고리야"+ck_ea_age);
+
 		//페이징
-		
 		int currentPage = 1;
 		if(request.getParameter("currentPage") != null) {
 			currentPage = Integer.parseInt(request.getParameter("currentPage"));
 		}
 		PageInfo pi = Page.PageInfo("대외활동", currentPage, "/main.ea");
-		
 		//페이징
-		ArrayList<Board> list = new ExternalService().selectList(pi); 
+		ArrayList<Board> list = new ExternalService().selectList(pi); //Notice만 들어갈 수 있는 ArrayList가 반환 될 것이다.
 
 		String page = null;
-		
-/*		if(result==0) 
-		{*/
-					if(list != null) {
-						page = "WEB-INF/views/External_Activities/대외활동검색메인.jsp";
-						request.setAttribute("list", list);
-						request.setAttribute("pi", pi);//페이징	
-					}else {
-						page = "WEB-INF/views/Common/errorPage.jsp";
-						request.setAttribute("msg", "대외활동 게시판 조회에 실패하였습니다.");
-					}
-/*				}
-		} else {
-			for(Board b : list){
-				if(b.getCgName()!=ea_category && b.getTcName()!=ck_ea_age && b.getLcName()!=ck_lc) {
-					if(list != null) {
-						page = "WEB-INF/views/External_Activities/대외활동검색메인.jsp";
-						request.setAttribute("list", list);
-						request.setAttribute("pi", pi);//페이징	
-						request.setAttribute("ea_category", ea_category);
-						request.setAttribute("ck_ea_age", ck_ea_age);
-						request.setAttribute("ck_lc", ck_lc);
-					}else {
-						page = "WEB-INF/views/Common/errorPage.jsp";
-						request.setAttribute("msg", "대외활동 게시판 조회에 실패하였습니다.");
-					}
+		for(Board b : list){
+			if(b.getCgName()==ea_category && b.getTcName()==ck_ea_age && b.getLcName()==ck_lc) {
+				if(list != null) {
+					page = "WEB-INF/views/External_Activities/대외활동검색메인.jsp";
+					request.setAttribute("list", list);
+					request.setAttribute("pi", pi);//페이징	
+					
+					request.setAttribute("ea_category", ea_category);
+					request.setAttribute("ck_ea_age", ck_ea_age);
+					request.setAttribute("ck_lc", ck_lc);
+					
+				}else {
+					page = "WEB-INF/views/Common/errorPage.jsp";
+					request.setAttribute("msg", "대외활동 게시판 조회에 실패하였습니다.");
+				}
+			}else {
+				if(list != null) {
+					page = "WEB-INF/views/External_Activities/대외활동검색메인.jsp";
+					request.setAttribute("list", list);
+					request.setAttribute("pi", pi);//페이징	
+				}else {
+					page = "WEB-INF/views/Common/errorPage.jsp";
+					request.setAttribute("msg", "대외활동 게시판 조회에 실패하였습니다.");
 				}
 			}
-		}*/
+		}
 		request.getRequestDispatcher(page).forward(request, response);
 	}
 
